b06563d4fec01ba296972c0d8c6b123daba959ec
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-13 18:55:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,1 +1,33 @@
+def my_hash
+my_hash = {"Name" => "Marine Boudeau", "Age" => "32", "Status" => "Single"}
+end  
 
+
+
+def shipping_manifest
+  
+shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+end
+def retrieval
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+shipping_manifest["oil paintings"]
+  
+end
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+                 
+    shipping_manifest["muskets"] = 2
+
+
+      shipping_manifest["gun powder"] = 4
+
+            shipping_manifest
+                            
+end
